fca6b3e7ecc89dcbc016ed52688d70e9a2401a4b
Jack Ault <henlowgg@outlook.com>
2023-07-20 15:55:43 -0400

    solution

--- banjo.js
+++ banjo.js
@@ -10,5 +10,7 @@
 
 function areYouPlayingBanjo(name) {
   // Implement me
-  return name;
+  const predicate = name[0].toLowerCase() === 'r' ? ' plays banjo' : ' does not play banjo';
+
+  return name + predicate;
 }
