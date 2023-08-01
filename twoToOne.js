046cdc0a7197baa3643736dcc37515960dbce653
Jack Ault <henlowgg@outlook.com>
2023-08-01 17:45:07 -0400

    return a new sorted string from 2 strings

---
+++ twoToOne.js
@@ -0,0 +1,26 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// Solution
+
+function longest(s1, s2) {
+    // your code
+  }
+
+// Sample Tests
+
+const Test = require('@codewars/test-compat');
+
+describe("longest",function() {
+it("Basic tests",function() {
+    Test.assertEquals(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+    Test.assertEquals(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+    Test.assertEquals(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+})})
