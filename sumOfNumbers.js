a2399716d6c0516ec899eebf298d9becf43eb9a9
Jack Ault <henlowgg@outlook.com>
2023-08-01 17:25:12 -0400

    find the sum of all intergers and return it

---
+++ sumOfNumbers.js
@@ -0,0 +1,31 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// Solution 
+
+function getSum(a, b) {
+    
+}
+
+// Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {         
+    assert.strictEqual(getSum(0,-1), -1);
+    assert.strictEqual(getSum(0, 1),  1);
+    assert.strictEqual(getSum(2, 2),  2);
+  })
+});
