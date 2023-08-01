e2cb996603ab51b916f3efee5675960d05688f26
Jack Ault <henlowgg@outlook.com>
2023-08-01 16:15:51 -0400

    square sum

---
+++ squareNSum.js
@@ -0,0 +1,40 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 
+// 1
+// 2
+// +
+// 2
+// 2
+// +
+// 2
+// 2
+// =
+// 9
+// 1 
+// 2
+//  +2 
+// 2
+//  +2 
+// 2
+//  =9.
+
+// Solution
+
+function squareSum(numbers) {
+    
+}
+
+// Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(squareSum([1,2]), 5);
+    assert.strictEqual(squareSum([0, 3, 4, 5]), 50);
+    assert.strictEqual(squareSum([]), 0);
+  });
+});
