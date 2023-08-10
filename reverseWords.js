6e8d3de38fcecb5f9a8ab505a81693bfe3c2b6b7
Jack Ault <henlowgg@outlook.com>
2023-08-10 13:06:52 -0400

    reverse words kata 7, complete function that accepts a string param and reverses each word in string

---
+++ reverseWords.js
@@ -0,0 +1,27 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// Solution
+
+function reverseWords(str) {
+    // Go for it
+}
+
+
+// Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
